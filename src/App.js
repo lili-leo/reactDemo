f1f81fb9fde2f573834a84dc0afe79c6d379edca
lili <lili@163.com>
2021-06-28 00:47:19 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const list=()=>{
+    return 
+    (<div>1111</div>)
+  }
+  state={
+    tex:''
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type="text" value={this.state.tex}
+        
+        ></input> 
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <list></list>
+      </header>
+    </div>
+  );
+}
+
+export default App;
